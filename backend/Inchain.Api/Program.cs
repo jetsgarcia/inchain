40c7281eb5ea0a41fd5eb025704164e3cf85d534
Jetter <jlgarcia2@student.apc.edu.ph>
2026-06-10 12:34:15 +0800

    fix: added auth role authorization for identity endpoints

--- backend/Inchain.Api/Program.cs
+++ backend/Inchain.Api/Program.cs
@@ -1,25 +1,33 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Inchain.Api.Data;
+using Inchain.Api.Features.Admin.Users.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Services
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddAuthorization();
-builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services
+    .AddIdentityApiEndpoints<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
+
+// Feature services
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
-// HTTP request pipeline.
+// HTTP request pipeline
 app.MapIdentityApi<IdentityUser>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
