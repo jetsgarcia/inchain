344269bdfe54354234d24fbef6dcbe1dba2a8573
Jetter <jlgarcia2@student.apc.edu.ph>
2026-06-10 11:58:38 +0800

    feat: add IUserService interface with CreateUserAsync method

---
+++ backend/Inchain.Api/Features/Admin/Users/Services/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Inchain.Api.Features.Admin.Users.Services;
+
+public interface IUserService
+{
+    Task<(IdentityResult Result, IdentityUser? User)> CreateUserAsync(string email, string password);
+}
